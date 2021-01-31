731537ed91d5477a8c1ecaa20283dad1b1a2ebb5
theodorous beatayuvens <theochandra27@gmail.com>
2021-01-31 22:24:06 +0700

    make product vm into parcelable

--- app/src/main/java/com/android/basicecommerce/presentation/model/ProductVM.kt
+++ app/src/main/java/com/android/basicecommerce/presentation/model/ProductVM.kt
@@ -1,5 +1,9 @@
 package com.android.basicecommerce.presentation.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class ProductVM(
     val id: Int,
     val imageUrl: String,
@@ -7,4 +11,4 @@ data class ProductVM(
     val description: String,
     val price: String,
     val loved: Int
-)
+) : Parcelable
