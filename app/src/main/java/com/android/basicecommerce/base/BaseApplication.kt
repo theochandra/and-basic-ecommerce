3dd1f3e6a5c56995e39d0acb22c98733848fdd7b
theodorous beatayuvens <theochandra27@gmail.com>
2021-01-31 13:37:23 +0700

    add base application

--- app/src/main/java/com/android/basicecommerce/base/BaseApplication.kt
+++ app/src/main/java/com/android/basicecommerce/base/BaseApplication.kt
@@ -1,4 +1,31 @@
 package com.android.basicecommerce.base
 
-class BaseApplication {
+import android.app.Application
+import com.android.basicecommerce.BuildConfig
+import com.android.basicecommerce.di.Injector
+import com.android.basicecommerce.di.application.*
+import com.android.basicecommerce.di.home.HomeSubComponent
+import com.android.basicecommerce.di.login.LoginSubComponent
+
+class BaseApplication : Application(), Injector {
+
+    private lateinit var appComponent: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(applicationContext))
+            .netModule(NetModule(BuildConfig.BASE_URL))
+            .remoteDataModule(RemoteDataModule())
+            .build()
+    }
+
+    override fun createLoginSubComponent(): LoginSubComponent {
+        return appComponent.loginSubComponent().create()
+    }
+
+    override fun createHomeSubComponent(): HomeSubComponent {
+        return appComponent.homeSubComponent().create()
+    }
+
 }
