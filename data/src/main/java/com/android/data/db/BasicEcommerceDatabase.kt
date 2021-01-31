62073c0f481279b3fe2979a67f60fbfeeefb99a5
theodorous beatayuvens <theochandra27@gmail.com>
2021-01-31 13:31:22 +0700

    add database instance

--- data/src/main/java/com/android/data/db/BasicEcommerceDatabase.kt
+++ data/src/main/java/com/android/data/db/BasicEcommerceDatabase.kt
@@ -1,4 +1,16 @@
 package com.android.data.db
 
-class BasicEcommerceDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.android.data.entity.Product
+
+@Database(
+    entities = [Product::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class BasicEcommerceDatabase : RoomDatabase() {
+
+    abstract fun basicEcommerceDao(): BasicEcommerceDao
+
 }
